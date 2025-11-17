9cf4661e5ad0ca1b8961ee1b9715993edd23f04a
GILGAMESH605 <mh6455@nyu.edu>
2025-11-17 01:07:10 -0500

    Update __init__.py

--- web-app/app/__init__.py
+++ web-app/app/__init__.py
@@ -1,1 +1,16 @@
-# create_app(), register blueprint
+from flask import Flask
+from flask_cors import CORS
+from .config import Config
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    app.config["MAX_CONTENT_LENGTH"] = Config.MAX_FILE_MB * 1024 * 1024
+    CORS(app)
+
+    # Register routes
+    from .routes import bp as routes_bp
+    app.register_blueprint(routes_bp)
+
+    return app
+
